ea64cb571b4f067ccbc42f4065b75cb353815edd
kajal <kajal@kajal>
2022-12-15 11:14:00 +0000

    Add project files.

---
+++ Railway_Reservation/Controllers/HomeController.cs
@@ -0,0 +1,131 @@
+﻿using Railway_Reservation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Railway_Reservation.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext db = new DataContext(); 
+   
+        public ActionResult Index()
+        {
+           
+
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User u)
+        {
+            db.Users.Add(u);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult afterlogin()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+       
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+
+
+               // var f_password = GetMD5(password);
+                var data = db.Users.Where(s => s.Email.Equals(email) && s.Password.Equals(password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    //Session["FullName"] = data.FirstOrDefault().FirstName + " " + data.FirstOrDefault().LastName;
+                    //Session["Email"] = data.FirstOrDefault().Email;
+                    //Session["UserId"] = data.FirstOrDefault().UserId;
+                    return RedirectToAction("afterlogin");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            
+            return View();
+        }
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AdminLogin(string email, string password,string role)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+
+
+                // var f_password = GetMD5(password);
+                var data = db.Users.Where(s => s.Email.Equals(email) && s.Password.Equals(password)&& s.Role.Equals(role)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    //Session["FullName"] = data.FirstOrDefault().FirstName + " " + data.FirstOrDefault().LastName;
+                    //Session["Email"] = data.FirstOrDefault().Email;
+                    //Session["UserId"] = data.FirstOrDefault().UserId;
+                    return RedirectToAction("Updatetrain");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("AdminLogin");
+                }
+            }
+
+            return View();
+        }
+
+
+
+        //Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
+        }
+        public ActionResult reserve()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult reserv(Reservation r)
+        {
+            db.Reservations.Add(r);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Updatetrain()
+        {
+            return View();
+        }
+    }
+}
---
+++ Railway_Reservation/Models/TrainDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Railway_Reservation.Models
+{
+    public class TrainDetails
+    {
+       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Train_Id { get; set; }
+        public string TrainName { get;set; }
+        public string SourceStation { get; set; }
+        public string DestinationStation { get; set; }
+        public int Fare { get; set; }
+        public string ArrivalTime { get; set; }
+        public string DepartureTime { get; set;}
+        public int TotalSeats { get; set;}
+    }
+}
